e1fa98c122958b2d78f56a0b50da37d3887a8374
Sup8045 <superworker895@gmail.com>
2021-07-05 05:58:43 +0000

    Pointers.c

---
+++ pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int x =4,*p = &x;
+    
+    printf("%d\n",*p);
+    
+    
+    
+    // * is the value of or dereferencing operator which will access the value in the location pointed by the
+//pointer
+    
+    
+    
+    
+}
